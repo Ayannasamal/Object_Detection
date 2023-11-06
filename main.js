e5927b40a2c0e3b551529f7e4bc4359897a91313
Ayannasamal <96683706+Ayannasamal@users.noreply.github.com>
2023-11-06 18:45:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 status1="";
+object = [];
 
 function preload(){
     img=loadImage("dog_cat.jpg");
@@ -14,17 +15,19 @@ function setup(){
 
 function draw(){
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
+    if(status1 != ""){
+        document.getElementById("status").innerHTML="Status : Detected objects";
+        for(i=0; i<object.length;i++){
+            fill("#FF0000");
+            percentage = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percentage + "%" , object[i].x+15 , object[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height); 
 
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
+        }
+    }
+    
 }
 
 function modelLoaded(){
@@ -39,5 +42,6 @@ function gotResult(error, results){
     }
     else{
         console.log(results);
+        object=results;
     }
 }
